85e2164bf07fc21dc1edc058c1b3a735ba88fe7f
Lee <kos1990l@gmail.com>
2012-06-12 16:39:34 +0800

    finish level 17

---
+++ level17/level17.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+'''
+question url: http://www.pythonchallenge.com/pc/return/romance.html
+'''
+
+import sys
+import os
+
+def call(name):
+    import xmlrpclib
+    xmlrpc = xmlrpclib.ServerProxy("http://www.pythonchallenge.com/pc/phonebook.php")
+    return xmlrpc.phone(name)
+
+info_file = 'level17.d/info'
+if os.path.exists(info_file):
+    f = open(info_file)
+    print f.read()
+    f.close()
+    print call('Leopold')
+
+    import urllib2
+    url = 'http://www.pythonchallenge.com/pc/stuff/violin.php'
+    info = 'the flowers are on their way'
+    req = urllib2.Request(url, headers={'Cookie': 'info='+info})
+    print urllib2.urlopen(req).read()
+    sys.exit(0)
+
+import re
+match = re.compile(r'\D+(\d+$)').findall
+url = 'http://www.pythonchallenge.com/pc/def/linkedlist.php?busynothing='
+
+default = '12345' if len(sys.argv) == 1 else sys.argv[1]
+busynothing = default
+pre = ''
+
+
+def next_busynothing_from(page):
+    content = page.read()
+    nums = match(content)
+    page.close()
+    return (nums[-1] if nums else None, content)
+
+
+def cookie_info_from(page):
+    req = page.info()
+    cookie = req.getheader('set-cookie')
+    info = cookie.split(';')[0]
+    return(info.split('=')[1])
+
+
+import urllib
+mesg = []
+haserr = False
+for i in xrange(400):
+    try:
+        page = urllib.urlopen(url + busynothing)
+        pre = busynothing
+        num, data = next_busynothing_from(page)
+        mesg.append(cookie_info_from(page))
+
+        if num:
+            busynothing = num
+            print i, busynothing, data
+        else:
+            print 'no match'
+            break
+    except KeyboardInterrupt:
+        print 'user interrupted'
+        haserr = True
+        break
+    except Exception, e:
+        print e
+        haserr = True
+        break
+
+if haserr:
+    print 'previous busynothing: %s' % pre
+else:
+    msg = ''.join(mesg)
+    #print msg
+    import bz2
+    msg = bz2.decompress(urllib.unquote_plus(msg))
+    f = open(info_file, 'w')
+    f.write(msg)
+    f.close()
+    print msg
